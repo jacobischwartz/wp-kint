3e3c9f5ef6270dc75770fbef642d9e48be4094d1
jschwartz <jschwartz@emfluence.com>
2024-06-03 15:33:09 -0500

    Basic working plugin

---
+++ plugin.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Wordpress Kint
+Plugin URI: https://github.com/jacobischwartz?tab=repositories
+Description: A simple Wordpress implementation of the Kint developer tool
+Author: Jacob Schwartz
+Version: 1.0
+Author URI: https://github.com/jacobischwartz
+Requires at least: 6.0
+Requires PHP: 8.0
+
+This plugin does not make any representations about the Kint library, which is developed by others and lives here:
+https://github.com/kint-php/kint/
+
+*/
+
+require_once 'kint.phar';
---
+++ readme.txt
@@ -0,0 +1,17 @@
+=== Wordpress Kint ===
+Contributors: jacobischwartz
+Tags: debug, kint
+Requires at least: 6.0
+Tested up to: 6.5
+Requires PHP: 8.0
+Stable tag: 1.0
+
+== Description ==
+
+A very simple wrapper for the Kint library.
+
+Use kint in your code very simply, like this:
+d($some_variable);
+
+For more fancy usages, refer to the Kint documentation here:
+https://kint-php.github.io/kint/#use
